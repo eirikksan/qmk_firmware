fad543a540faad9f140d212838c0f2f39632424e
eirikksan <109383175+eirikksan@users.noreply.github.com>
2025-04-02 12:08:18 +0200

    Update keyboard (#3)
    * Delete keyboards/mechboards/crkbd/pro/keymaps/eirkan directory
    
    * Create mt.txt
    
    * Add files via upload

--- keyboards/mechboards/crkbd/pro/keymaps/eirikksan/keymap.c
+++ keyboards/mechboards/crkbd/pro/keymaps/eirikksan/keymap.c
@@ -9,23 +9,23 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, NO_ARNG,
-    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,   NO_OSTR, KC_ESC,
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM, KC_DOT, KC_MINS, KC_BSPC,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,   NO_OSTR, KC_ESC,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM, KC_DOT, NO_MINS, KC_BSPC,
                                         KC_LGUI, MO(1),  KC_SPC,      KC_ENT,   MO(2), KC_LALT
 
 ),
 
   [1] = LAYOUT_split_3x6_3(
-    KC_TAB,  XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,
-    KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX,
-    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_0, KC_1, KC_2, KC_3, KC_DEL,
+    KC_TAB,  XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,                       NO_PLUS, KC_7, KC_8, KC_9, XXXXXXX, XXXXXXX,
+    KC_LCTL, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, NO_BSLS,                       NO_MINS, KC_4, KC_5, KC_6, XXXXXXX, XXXXXXX,
+    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NO_SLSH,                       KC_0, KC_1, KC_2, KC_3,XXXXXXX, KC_DEL,
                                         KC_LGUI, _______, KC_SPC,      KC_ENT,  MO(3),   KC_LALT
 ),
 
   [2] = LAYOUT_split_3x6_3(
-    KC_TAB,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT,                       KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
-    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+    KC_TAB,  XXXXXXX,   NO_AT, NO_HASH,  NO_DLR, NO_PERC,                       NO_CIRC, NO_AMPR, NO_ASTR, NO_TILD, XXXXXXX, XXXXXXX,
+    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, NO_EXLM, NO_QUOT,                       NO_EQL, NO_LPRN,  NO_LBRC, NO_LCBR, NO_GRV, XXXXXXX,
+    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, NO_LABK, NO_DQUO,                       NO_UNDS, NO_RPRN, NO_RBRC, NO_RCBR, NO_PIPE, KC_BSPC,
                                         KC_LGUI, MO(3),   KC_SPC,      KC_ENT,  _______, KC_LALT
 ),
 
