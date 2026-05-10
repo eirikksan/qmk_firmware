6a3bd3e879de9ea4be71321022edca950d19f8fa
Eirik Kaasbøll Andresen <eirika_96@hotmail.com>
2026-05-10 21:31:24 +0200

    Refactor Eirkan keymap layout for crkbd: update key assignments and clean up unused layers

--- keyboards/mechboards/crkbd/pro/keymaps/eirkan/keymap.c
+++ keyboards/mechboards/crkbd/pro/keymaps/eirkan/keymap.c
@@ -11,38 +11,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, NO_ARNG,
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,   NO_OSTR, NO_AE,
     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM, KC_DOT, NO_MINS, KC_BSPC,
-                                        KC_LGUI, MO(1),  KC_SPC,      KC_ENT,   MO(2), KC_LALT
+                                        KC_LGUI, MO(1),  KC_SPC,      KC_ENT,   KC_RALT, KC_LALT
 
 ),
 
   [1] = LAYOUT_split_3x6_3(
-    KC_ESC,  XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,                       NO_PLUS, KC_7, KC_8, KC_9, XXXXXXX, XXXXXXX,
-    KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, NO_BSLS,                       NO_MINS, KC_4, KC_5, KC_6, XXXXXXX, XXXXXXX,
-    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NO_SLSH,                       KC_0, KC_1, KC_2, KC_3,XXXXXXX, KC_DEL,
-                                        KC_LGUI, _______, KC_SPC,      KC_ENT,  MO(3),   KC_LALT
+    KC_TAB,  XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,                       NO_PLUS, KC_7, KC_8, KC_9, XXXXXXX, XXXXXXX,
+    KC_LSFT, XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, NO_BSLS,                      NO_MINS, KC_4, KC_5, KC_6, XXXXXXX, XXXXXXX,
+    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NO_SLSH,                       LT(2,KC_0),KC_1, KC_2, KC_3, XXXXXXX, KC_DEL,
+                                        KC_LGUI, _______, KC_SPC,      KC_ENT,  KC_RALT,   KC_LALT
 ),
 
-  [2] = LAYOUT_split_3x6_3(
-    KC_TAB,  XXXXXXX,   NO_AT, NO_HASH,  NO_DLR, NO_PERC,                       NO_CIRC, NO_AMPR, NO_ASTR, NO_TILD, XXXXXXX, XXXXXXX,
-    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, NO_EXLM, NO_QUOT,                       NO_EQL, NO_LPRN,  NO_LBRC, NO_LCBR, NO_GRV, XXXXXXX,
-    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, NO_LABK, NO_DQUO,                       NO_UNDS, NO_RPRN, NO_RBRC, NO_RCBR, NO_PIPE, KC_BSPC,
-                                        KC_LGUI, MO(3),   KC_SPC,      KC_ENT,  _______, KC_LALT
-),
-
-  [3] = LAYOUT_split_3x6_3(
-    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    RM_NEXT, RM_HUED, RM_SATD, RM_VALD, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                        KC_LGUI, _______, KC_SPC,      KC_ENT,  _______, KC_LALT
+[2] = LAYOUT_split_3x6_3(
+  QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F12, XXXXXXX,
+  RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F11, XXXXXXX,
+  RM_NEXT, RM_HUED, RM_SATD, RM_VALD, XXXXXXX, XXXXXXX,                       _______, KC_F1, KC_F2, KC_F3, KC_F10, XXXXXXX,
+                                      KC_LGUI, XXXXXXX, KC_SPC,      KC_ENT,  KC_RALT, KC_LALT
 )
+//   [3] = LAYOUT_split_3x6_3(
+//     KC_ESC , XXXXXXX, NO_AT,   NO_HASH, NO_DLR,  NO_PERC,                       NO_CIRC, NO_AMPR, NO_ASTR, NO_TILD, XXXXXXX, XXXXXXX,
+//     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, NO_EXLM, NO_QUOT,                       NO_EQL,  NO_LPRN, NO_LBRC, NO_LCBR, NO_GRV, XXXXXXX,
+//     KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, NO_LABK, NO_DQUO,                       NO_UNDS, NO_RPRN, NO_RBRC, NO_RCBR, NO_PIPE, XXXXXXX,
+//                                         KC_LGUI, MO(3),   KC_SPC,      KC_ENT,  _______, KC_LALT
+// )
+
 };
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT)},
 [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-[2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-[3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}
+[2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}
+// [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}
 };
 #endif
 
